6ba53e2cff60df9ee20034652b018f6fd1a8abe7
Rushil <rushil13579@gmail.com>
2025-08-06 01:16:13 +0530

    [IMP] chevron-left: navigating to '/' instead of '-1'. While creating a new post it redirects to that new post, and if clicks on back button it redirects to the create-post which doesn't seems to be a good option

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -99,7 +99,7 @@ function PostDetail() {
 
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-[#0e1113] py-8 px-4">
-      <button onClick={() => navigate(-1)} className="text-indigo-600 dark:text-indigo-400 flex items-center mb-6">
+      <button onClick={() => navigate('/')} className="text-indigo-600 dark:text-indigo-400 flex items-center mb-6">
         <ChevronLeft className="w-5 h-5 mr-1" />
         <span className="font-mono">Back</span>
       </button>
